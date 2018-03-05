5d8072224687f0214e465694b2203635c2bc2613
lipengfei <437436270@qq.com>
2018-03-05 10:52:42 +0800

    second commit

---
+++ minerecyclerviewmodule/src/main/java/com/example/minerecyclerviewmodule/view/MyReclcyerView.java
@@ -0,0 +1,25 @@
+package com.example.minerecyclerviewmodule.view;
+
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+
+/**
+ * Created by LPF on 2018/3/5.
+ */
+
+public class MyReclcyerView extends RecyclerView {
+    public MyReclcyerView(Context context) {
+        super(context);
+    }
+
+    public MyReclcyerView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyReclcyerView(Context context, @Nullable AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+}
